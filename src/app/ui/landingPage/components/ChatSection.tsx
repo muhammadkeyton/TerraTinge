763f80252b658c92c9defa9621c8bbf716e34e3a
Josh <119835493+josh76543210@users.noreply.github.com>
2024-04-15 21:34:26 -0700

    Add transition for Chat button;

--- src/app/ui/landingPage/components/ChatSection.tsx
+++ src/app/ui/landingPage/components/ChatSection.tsx
@@ -3,8 +3,8 @@ import ChatIcon from '@mui/icons-material/Chat';
 
 export default function ChatSection() {
   return (
-    <Button className='fixed bottom-5 right-5 group transition-all bg-black text-white dark:text-black dark:bg-white flex rounded-full'>
-      <div className='text-sm text-left w-0 h-0 overflow-hidden group-hover:pl-7 group-hover:w-auto group-hover:h-auto flex flex-col gap-1 justify-center'>
+    <Button className='fixed bottom-5 right-5 group bg-black text-white dark:text-black dark:bg-white flex rounded-full'>
+      <div className='text-sm text-left w-0 h-0 overflow-hidden group-hover:pl-7 group-hover:w-auto group-hover:h-auto flex flex-col gap-1 justify-center transition-all'>
         <h2 className='font-semibold'>Need help?</h2>
         <p>Click to chat with us!</p>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   darkMode: 'selector',
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
- 
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+
   theme: {
     extend: {
       fontFamily: {
-        'app': ['Montserrat', 'sans-serif'],
-        'instagram':['Lobster Two','sans-serif']
+        app: ['Montserrat', 'sans-serif'],
+        instagram: ['Lobster Two', 'sans-serif'],
       },
       colors: {
-        'black':'#000000',
-        'white': '#ffffff',
-        
+        black: '#000000',
+        white: '#ffffff',
       },
       animation: {
         'ping-slow': 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite',
       },
-     
     },
   },
   plugins: [],
