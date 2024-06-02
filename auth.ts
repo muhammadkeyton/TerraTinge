3eb65704e3b3695852e747793a4f3bf01d68bdb5
muhammadkeyton <muhammadkeyton@gmail.com>
2024-06-02 11:10:00 -0400

    add nextauth secret

--- auth.ts
+++ auth.ts
@@ -15,12 +15,19 @@ import Google from "next-auth/providers/google"
 
 import Resend from "next-auth/providers/resend"
 
-
+import { cert } from "firebase-admin/app"
 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: FirestoreAdapter(),
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY
+    }),
+  }),
   session:{strategy:'jwt'},
+  secret:process.env.AUTH_SECRET,
 
   ...authConfig,
   
