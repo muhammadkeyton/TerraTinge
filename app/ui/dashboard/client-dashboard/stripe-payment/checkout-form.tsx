5fc862f05c73e1c34aa1d7b70303fec7fa1c63ab
muhammadkeyton <muhammadkeyton@gmail.com>
2024-07-25 20:11:16 -0400

    add experimental stripe paymentcomponents and api

--- app/ui/dashboard/client-dashboard/stripe-payment/checkout-form.tsx
+++ app/ui/dashboard/client-dashboard/stripe-payment/checkout-form.tsx
@@ -67,7 +67,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/dashboard",
+        return_url: "https://terra-tinge.vercel.app/dashboard",
       },
     });
 
