80caa821b63b760a8b46be990e257ac87ab3a240
muhammadkeyton <muhammadkeyton@gmail.com>
2024-09-01 20:46:37 -0400

    add sitemap and metatags

--- app/authentication/page.tsx
+++ app/authentication/page.tsx
@@ -1,6 +1,11 @@
 import LoginPage from "@/app/ui/login-page/login";
 
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: "TerraTinge Login",
+    description: "Login to submit a project description and build your dream app with us",
+};
 
 export default function Authentication(){
     return <LoginPage/>
--- app/partnership/page.tsx
+++ app/partnership/page.tsx
@@ -1,5 +1,12 @@
 import PartnershipLoginPage from "@/app/ui/partnership-login-page/partnership-login-page";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "TerraTinge Partnership",
+    description: "Become our partner,create a partner account and start earning with us",
+};
+
 export default function PartnerShip(){
     return (
         <PartnershipLoginPage/>
